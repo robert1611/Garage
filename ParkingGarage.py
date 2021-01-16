722a70b324fe732247da2d7ad3b982fec31b4600
robert1611 <76051413+robert1611@users.noreply.github.com>
2021-01-16 14:29:03 -0600

    Add files via upload
    2:28 CST 16-Jan

--- ParkingGarage.py
+++ ParkingGarage.py
@@ -1,66 +1,73 @@
-#Class: ParkingGarage
-
-#overall the logic seems sloppy.  Why would you have a ticket AND a current 
-
 class ParkingGarage:
-    def __init__(self, amountTickets):
-        self.amountTickets = amountTickets
-        self.tickets = [False] * amountTickets   # False = Available, True = Not Available
-        self.parkingSpaces = [False] * amountTickets # False = Available, True = Not Available
-        self.currentTicket = {}   #dictionary for a current log for who pays, enters, leaves
-        for i in range(amountTickets):
-            self.currentTicket[i] = False
-    
-    # takeTicket will output a ticket upon request as long as currentTicket <= 10.  This will decrement both parkingSpaces an currentTicket by 1
+    """# parkingSpaces = 0
+    # tickets = 10
+    # currentTicket = {
+    #     "paid": False,
+    # }"""
+
+    def __init__(self):
+
+        self.parkingSpaces = 0
+        self.tickets = 10
+        self.currentTicket = {
+            "paid": False,
+        }
+
+    # takeTicket will output a ticket upon request as long as currentTicket <= 10.
     def takeTicket(self):
-        input("Press enter to take a ticket")
-        spots_available = 0
-        for ticket in self.tickets:
-            if ticket == False:
-                spots_available = spots_available + 1
-        if spots_available > 0:
-            #(1) decrement ticket and (2) decrement parking space
-            for ticket_number in range(self.amountTickets):
-                if self.tickets[ticket_number] == False:
-                    self.tickets[ticket_number] = True
-                    self.parkingSpaces[ticket_number] = True
-                    break
-            return "Welcome."
+        if self.parkingSpaces < 10 and self.tickets > 0:
+            print("you can enter")
+            self.parkingSpaces = self.parkingSpaces + 1
+            self.tickets = self.tickets - 1
+
+            # to remove
+            print(self.tickets)
+            print(self.parkingSpaces)
+
         else:
-            return "We're sorry.  The parking garage is full."
+            print("We're sorry. No Parking available")
+
+            # to remove
+            print(self.tickets)
+            print(self.parkingSpaces)
 
     # Display an input that waits for an amount from the user and stores it in a variable
     def payForParking(self):
-        payment = input("Please enter something to pay for your parking")
+        payment = input("Enter the payment: ")
         if payment != "":
-            print("Your ticket has been paid.  Please exit within 15 minutes")  #The print statement is nest within the if statement, so it should execute
+            # The print statement is nest within the if statement, so it should execute
+            print("Have a nice day")
             self.currentTicket["paid"] = True
         else:
-            print("Your ticket has NOT been paid.")  #The print statement is nest within the if statement, so it should execute
-    
+            # The print statement is nest within the if statement, so it should execute
+            print("Your ticket has NOT been paid.")
+
     def leaveGarage(self):
-        if self.currentTicket["paid"] == True:
-            print("Thank you and have a nice day")
-            #(1) increment ticket and (2) increase parking space
-        for ticket_number in range(self.amountTickets):
-            if self.tickets[ticket_number] == True:
-                self.tickets[ticket_number] = False
-                self.parkingSpaces[ticket_number] = False
-                break
+        if self.currentTicket["paid"] != True:
+            print("Please pay first!!!")
+            return
 
-# Instantiate.
+        if self.parkingSpaces > 0:
+            self.parkingSpaces = self.parkingSpaces - 1
+            self.tickets = self.tickets + 1
+            print("you can leave")
+            self.currentTicket["paid"] = False
+        else:
+            print("Are you sure you're parked here.  The garage is empty !!!")
 
-parkingGarage = ParkingGarage(10)
-result = parkingGarage.takeTicket()
-print(result)
-result = parkingGarage.takeTicket()
-print(result)
-result = parkingGarage.takeTicket()
-print(result)
-result = parkingGarage.takeTicket()
-print(result)
-result = parkingGarage.takeTicket()
-print(result)
+#make sure this is outside of the class loop in order to run
+def run():
+    parkingGarage = ParkingGarage()
+    while True:
+        response = input("What do you want to enter, pay or leave?  ")
+        if (response.lower() == 'enter'):
+            parkingGarage.takeTicket()
+        elif (response.lower() == 'pay'):
+            parkingGarage.payForParking()
+        elif (response.lower() == 'leave'):
+            parkingGarage.leaveGarage()
+        else:
+            ("Invalid response")
 
-parkingGarage.payForParking()
-parkingGarage.leaveGarage()
+
+run()
